a1d49a420e4a2c20e79d41aaca367a79d301eb88
emilyboo <ehbookstein@gmail.com>
2014-10-04 14:51:51 -0700

    Use reduce() in mult_numbers. Takes 2 numbers and multiplies using reduce.

--- skills1.py
+++ skills1.py
@@ -67,10 +67,13 @@ def sum_numbers(number_list):
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(number_list):
-    total = number_list[0]
-    for num in number_list[1:]:
-        total = total * num
-    return total
+    # total = number_list[0]
+    # for num in number_list[1:]:
+    #     total = total * num
+    # return total
+    def multiply(num1, num2):
+        return num1 * num2
+    return reduce(multiply, number_list)
 
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
 def join_strings(word_list):
