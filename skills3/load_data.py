7a5a68ef4587c71ad88e1ec7c99c7304d8507005
emilyboo <ehbookstein@gmail.com>
2014-10-18 16:07:00 -0700

    Added CREATE TABLE to functions that create customers and orders tables in load_data, containing all info needed to recreate tables as needed

--- skills3/load_data.py
+++ skills3/load_data.py
@@ -7,10 +7,37 @@ def connect_to_db():
 
 	DB = CONN.cursor()
 
+# drop existing database tables
+# DOES NOT WORK!?
+# def delete_table(tablename):
+# 	query = """DROP TABLE ?"""
+# 	DB.execute(query, (tablename, ))
+
 # Make the database from CSV files
 
 def populate_customers_table(filename):
 	f = open(filename)
+
+	DB.execute("""
+		CREATE TABLE customers (id INTEGER NOT NULL,
+		email VARCHAR(120) NOT NULL,
+		givenname VARCHAR(30),
+		surname VARCHAR(30),
+		password VARCHAR(200),
+		telephone VARCHAR(20),
+		called DATE,
+		tos_agree INTEGER,
+		gender VARCHAR(13),
+		dob DATE,
+		billto_address1 VARCHAR(40),
+		shipto_address2 VARCHAR(40),
+		shipto_city VARCHAR(35),
+		shipto_state VARCHAR(35),
+		shipto_postalcode VARCHAR(10),
+		region VARCHAR(20),
+		PRIMARY KEY (id),
+		CHECK (gender IN ('Not Specified', 'Male', 'Female', 'Other')));
+		""")
 	#table column names must be specified AND in order!! See .schema
 	#I also wasn't supposed to include the id number. Why??
 	query = """INSERT INTO customers (email, givenname, surname,
@@ -31,6 +58,27 @@ def populate_customers_table(filename):
 
 def populate_orders_table(filename):
 	f = open(filename)
+
+	DB.execute("""
+		CREATE TABLE orders (
+		id INTEGER NOT NULL,
+		customer_id INTEGER NOT NULL,
+		status VARCHAR(16),
+		salesperson_id INTEGER,
+		shipto_address1 VARCHAR(40),
+		shipto_address2 VARCHAR(40),
+		shipto_city VARCHAR(35),
+		shipto_state VARCHAR(35),
+		shipto_postalcode VARCHAR(10),
+		subtotal NUMERIC(10, 2),
+		tax NUMERIC(10, 2),
+		delivery_method VARCHAR(20),
+		delivery_amount NUMERIC(10, 2),
+		order_total NUMERIC(10, 2), delivered_at INTEGER,
+		PRIMARY KEY (id),
+		CHECK (status IN ('New', 'Processing', 'Out for Delivery', 'Delivered', 'Canceled'))
+		);
+		""")
 
 	query = """INSERT INTO orders (customer_id, status, shipto_address1, shipto_city,
 		shipto_state, shipto_postalcode, subtotal, tax, order_total)
@@ -55,7 +103,7 @@ def populate_orders_table(filename):
 
 def main():
 	connect_to_db()
-	# populate_customers_table("customers.csv")
+	populate_customers_table("customers.csv")
 	populate_orders_table("orders.csv")
 
 if __name__ == "__main__":
