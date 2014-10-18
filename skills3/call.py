f4135107892c9289ddcb59f59c5e0fb7df823930
emilyboo <ehbookstein@gmail.com>
2014-10-18 16:36:38 -0700

    IT WORKS... changed update_customer_called to add value to customer.called (also now included in Customer class). Added date module to get today's date.

--- skills3/call.py
+++ skills3/call.py
@@ -10,21 +10,25 @@ call.py - Telemarketing script that displays the next name
 """
 
 import sqlite3
+from datetime import date
 
 DB = None
 CONN = None
 
 #Class definition to store our customer data
 class Customer(object):
-	def __init__(self, id=None, first=None, last=None, telephone=None):
+	def __init__(self, id=None, first=None, last=None, telephone=None, called=None):
+		self.id = ''
 		self.first = ''
 		self.last = ''
 		self.telephone = ''
-		pass
+		self.called = ''
 
 	def __str__(self):
 		output = " Name: %s, %s\n" % (self.last, self.first)
-		output += "Phone: %s" % self.telephone
+		output += "Phone: %s\n" % self.telephone
+		output += "ID: %s\n" % self.id
+		output += "Last called: %s" % self.called
 
 		return output
 
@@ -46,11 +50,13 @@ def get_next_customer(c_id):
 	query = """SELECT * FROM customers WHERE id = ?"""
 	DB.execute(query, (c_id, ))
 	row = DB.fetchone()
+	c.id = row[0]
 	c.first = row[2]
 	c.last = row[3]
 	c.telephone = row[5]
+	c.called = row[6]
 
-	print c
+	return c
 
 
 def display_next_to_call(customer):
@@ -63,26 +69,34 @@ def display_next_to_call(customer):
 
 # Update the "last called" column for the customer
 #   in the database.
-def update_customer_called(customer):
-	pass
+def update_customer_called(date, customer):
+	query = """UPDATE customers SET called = ? WHERE id = ?"""
+	DB.execute(query, (date, customer.id))
+	customer.called = date
+	print customer
+	CONN.commit()
 
 def main():
 	connect_to_db()
 
 	done = False
+	c_id = 0
+	today = date.today().strftime("%m/%d/%Y")
+	# mimic 04/01/2014 for date called
+	print today
 
-	# while not done:
-	c_id = 1
-	customer = get_next_customer(c_id)
-		# display_next_to_call(customer)
+	while not done:
+		c_id += 1
+		customer = get_next_customer(c_id)
+		display_next_to_call(customer)
 
-		# print "Mark this customer as called?"
-		# user_answer = raw_input('(y/n) > ')
+		print "Mark this customer as called?"
+		user_answer = raw_input('(y/n) > ')
 
-		# if user_answer.lower() == 'y':
-		# 	update_customer_called(customer)
-		# else:
-		# 	done = True
+		if user_answer.lower() == 'y':
+			update_customer_called(today, customer)
+		else:
+			done = True
 
 
 if __name__ == '__main__':
