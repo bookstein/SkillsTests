069c9e256456e34b69c20b6cda74108a96687bf7
emilyboo <ehbookstein@gmail.com>
2014-10-18 14:26:27 -0700

    Populated db customers table using function in python. Had to remove 'id' from query string for itto work.

--- skills3/call.py
+++ skills3/call.py
@@ -16,71 +16,85 @@ CONN = None
 
 # Make the database from CSV files
 
-def populate_database(filename):
-	query = """INSERT INTO
+def populate_customers_table(filename):
+	f = open(filename)
+	#table column names must be specified AND in order!! See .schema
+	#I also wasn't supposed to include the id number. Why??
+	query = """INSERT INTO customers (email, givenname, surname,
+		telephone, called) VALUES (?,?,?,?,?)"""
+	for line in f:
+		customer_record = line.strip().split(",")
+		cid, first, last, email, telephone, called = customer_record
+		if len(customer_record[5]) < 1:
+			customer_record[5] = "00/00/00"
+		print "Row: ", customer_record
+		DB.execute(query, (email, first, last, telephone, called))
+	CONN.commit()
+	f.close()
 
-# Class definition to store our customer data
-class Customer(object):
-	def __init__(self, id=None, first=None, last=None, telephone=None):
-		self.first = ''
-		self.last = ''
-		self.telephone = ''
-		pass
+# # Class definition to store our customer data
+# class Customer(object):
+# 	def __init__(self, id=None, first=None, last=None, telephone=None):
+# 		self.first = ''
+# 		self.last = ''
+# 		self.telephone = ''
+# 		pass
 
-	def __str__(self):
-		output = " Name: %s, %s\n" % (self.last, self.first)
-		output += "Phone: %s" % self.telephone
+# 	def __str__(self):
+# 		output = " Name: %s, %s\n" % (self.last, self.first)
+# 		output += "Phone: %s" % self.telephone
 
-		return output
+# 		return output
 
 # Connect to the Database
 def connect_to_db():
 	global DB, CONN
-	CONN = sqlite3.connect('melons.db')
+	CONN = sqlite3.connect('meloninfo.db')
 
 	DB = CONN.cursor()
 
 
-# Retrieve the next uncontacted customer record from the database.
-# Return the data in a Customer class object.
-#
-# Remember: Our telemarketers should only be calling customers
-#           who have placed orders of 20 melons or more.
-def get_next_customer():
-	c = Customer()
+# # Retrieve the next uncontacted customer record from the database.
+# # Return the data in a Customer class object.
+# #
+# # Remember: Our telemarketers should only be calling customers
+# #           who have placed orders of 20 melons or more.
+# def get_next_customer():
+# 	c = Customer()
 
-	return c
-
-
-def display_next_to_call(customer):
-	print "---------------------"
-	print "Next Customer to call"
-	print "---------------------\n"
-	print customer
-	print "\n"
+# 	return c
 
 
-# Update the "last called" column for the customer
-#   in the database.
-def update_customer_called(customer):
-	pass
+# def display_next_to_call(customer):
+# 	print "---------------------"
+# 	print "Next Customer to call"
+# 	print "---------------------\n"
+# 	print customer
+# 	print "\n"
+
+
+# # Update the "last called" column for the customer
+# #   in the database.
+# def update_customer_called(customer):
+# 	pass
 
 def main():
 	connect_to_db()
+	populate_customers_table("customers.csv")
 
-	done = False
+	# done = False
 
-	while not done:
-		customer = get_next_customer()
-		display_next_to_call(customer)
+	# while not done:
+	# 	customer = get_next_customer()
+	# 	display_next_to_call(customer)
 
-		print "Mark this customer as called?"
-		user_answer = raw_input('(y/n) > ')
+	# 	print "Mark this customer as called?"
+	# 	user_answer = raw_input('(y/n) > ')
 
-		if user_answer.lower() == 'y':
-			update_customer_called(customer)
-		else:
-			done = True
+	# 	if user_answer.lower() == 'y':
+	# 		update_customer_called(customer)
+	# 	else:
+	# 		done = True
 
 
 if __name__ == '__main__':
