0e4cccabc469cd33f4e2798a76ddf66d8df936da
emilyboo <ehbookstein@gmail.com>
2014-10-18 14:53:08 -0700

    Move INSERT query functions to separate python script for loading data

--- skills3/call.py
+++ skills3/call.py
@@ -14,25 +14,7 @@ import sqlite3
 DB = None
 CONN = None
 
-# Make the database from CSV files
-
-def populate_customers_table(filename):
-	f = open(filename)
-	#table column names must be specified AND in order!! See .schema
-	#I also wasn't supposed to include the id number. Why??
-	query = """INSERT INTO customers (email, givenname, surname,
-		telephone, called) VALUES (?,?,?,?,?)"""
-	for line in f:
-		customer_record = line.strip().split(",")
-		cid, first, last, email, telephone, called = customer_record
-		if len(customer_record[5]) < 1:
-			customer_record[5] = "00/00/00"
-		print "Row: ", customer_record
-		DB.execute(query, (email, first, last, telephone, called))
-	CONN.commit()
-	f.close()
-
-# # Class definition to store our customer data
+# #Class definition to store our customer data
 # class Customer(object):
 # 	def __init__(self, id=None, first=None, last=None, telephone=None):
 # 		self.first = ''
@@ -54,11 +36,11 @@ def connect_to_db():
 	DB = CONN.cursor()
 
 
-# # Retrieve the next uncontacted customer record from the database.
-# # Return the data in a Customer class object.
-# #
-# # Remember: Our telemarketers should only be calling customers
-# #           who have placed orders of 20 melons or more.
+# #Retrieve the next uncontacted customer record from the database.
+# #Return the data in a Customer class object.
+#
+# Remember: Our telemarketers should only be calling customers
+#           who have placed orders of 20 melons or more.
 # def get_next_customer():
 # 	c = Customer()
 
@@ -80,7 +62,6 @@ def connect_to_db():
 
 def main():
 	connect_to_db()
-	populate_customers_table("customers.csv")
 
 	# done = False
 
