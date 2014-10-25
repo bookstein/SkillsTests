e6317cc6ab199daec4a0a746a7d6512b0f1370e1
emilyboo <ehbookstein@gmail.com>
2014-10-25 11:54:46 -0700

    Create make_tables functions, connect_to_db function. Work on populating customer table with info from CSV. Tricky part was formatting 'called' column as DATE object.

--- skills3/load_data.py
+++ skills3/load_data.py
@@ -1,9 +1,19 @@
-import sqlite3
+import sqlite3 as sq3
+from datetime import date, datetime
+
+CONN = None
+DB = None
+
+def connect_to_db():
+	global CONN, DB
+	CONN = sq3.connect("meloninfo.db")
+	DB = CONN.cursor()
 
 
+def make_customer_table():
 
-	DB.execute("""
-		CREATE TABLE customers (id INTEGER NOT NULL,
+	DB.execute("""CREATE TABLE customers (
+		id INTEGER NOT NULL,
 		email VARCHAR(120) NOT NULL,
 		givenname VARCHAR(30),
 		surname VARCHAR(30),
@@ -20,10 +30,37 @@ import sqlite3
 		shipto_postalcode VARCHAR(10),
 		region VARCHAR(20),
 		PRIMARY KEY (id),
-		CHECK (gender IN ('Not Specified', 'Male', 'Female', 'Other')));
-	""")
+		CHECK (gender IN ('Not Specified', 'Male', 'Female', 'Other'))
+		);
+		""")
+	CONN.commit()
 
+def populate_customer_table():
+	f = open("customers.csv")
 
+	for line in f:
+		customer_id, first, last, email, telephone, called = line.strip().split(",")
+		#CSV data: customer_id,first,last,email,telephone,called
+
+		# if called == "":
+		# 	called = datetime.strptime("01/01/1970", "%m/%d/%Y")
+		if len(called) == 10:
+			called = datetime.strptime(called, "%m/%d/%Y")
+		else:
+			called = date(1970, 01, 01)
+
+		print called
+
+		# print customer_id, first, last, email, telephone, called
+
+	# 	query = """INSERT INTO customers (id, email, givenname, surname, telephone,
+	# 		called) VALUES (?, ?, ?, ?, ?, ?)"""
+	# 	DB.execute(query, (customer_id, email, first, last, telephone, called))
+
+	# CONN.commit()
+	f.close()
+
+def make_orders_table():
 	DB.execute("""
 		CREATE TABLE orders (
 		id INTEGER NOT NULL,
@@ -46,10 +83,16 @@ import sqlite3
 		CHECK (status IN ('New', 'Processing', 'Out for Delivery', 'Delivered', 'Canceled'))
 		);
 		""")
+	CONN.commit()
 
 
 
 def main():
+	connect_to_db()
+	# make_customer_table()
+	populate_customer_table()
+	# make_orders_table()
+	CONN.close()
 
 
 if __name__ == "__main__":
