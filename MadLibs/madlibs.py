dbdb3d11e4d438162acc5ae1bf996eb72f3c294f
emilyboo <ehbookstein@gmail.com>
2014-10-26 12:57:35 -0700

    Add form.reset() to ajax JS. Add if-compliments for loop in Jinja to index.html. Run a function that selects random compliments in Python handler. Had to provide else statement for if compliment checkbox was not checked (returns as None).

--- MadLibs/madlibs.py
+++ MadLibs/madlibs.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from random import choice, sample
 
 app = Flask(__name__)
+
+COMPLIMENTS = ["pretty", "smart", "joyful", "gracious"]
 
 @app.route("/")
 def display_home():
@@ -11,8 +14,21 @@ def display_home():
 def display_madlib():
 	name = request.args.get("person")
 	adjective = request.args.get("adjective")
-	return render_template("index.html", name=name, adjective=adjective)
+	wants_compliments = request.args.get("compliments")
 
+	if not name or not adjective:
+		return "Please fill out all fields!"
+
+	if wants_compliments:
+		compliment_list = make_compliments()
+	else:
+		compliment_list = None
+
+	return render_template("index.html", name=name, adjective=adjective, compliments=compliment_list)
+
+def make_compliments():
+	compliments = sample(COMPLIMENTS, 3)
+	return compliments
 
 if __name__ == "__main__":
 	app.run(debug=True)
