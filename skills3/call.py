62b823b61fdbdd0489273b5732b949d83cf89391
emilyboo <ehbookstein@gmail.com>
2014-10-25 20:05:57 -0700

    Write get_next_customer function, returns customer instance, then passed to display function in main()

--- skills3/call.py
+++ skills3/call.py
@@ -8,11 +8,12 @@ call.py - Telemarketing script that displays the next name
           an order of over 20 Watermelons.
 
 """
-
+from load_data import connect_to_db
 
 #Class definition to store our customer data
 class Customer(object):
-	def __init__(self, first, last, email, telephone, called):
+	def __init__(self, id, first, last, email, telephone, called):
+		self.id = id
 		self.first = first
 		self.last = last
 		self.email = email
@@ -23,9 +24,6 @@ class Customer(object):
 		return "Name: %s %s\n Telephone: %s\n Last called: %s" % (first, last,
 			 telephone, called)
 
-# Connect to the Database
-def connect_to_db():
-
 
 #Retrieve the next uncontacted customer record from the database.
 #Return the data in a Customer class object.
@@ -35,10 +33,16 @@ def connect_to_db():
 
 
 def get_next_customer():
-
-		# sample row
-		# 1|julia@roomm.gov|Linda|Garrett||8-(184)172-2138|10/19/2014||||||||||2354|1|Canceled||89 Cody Court||Woodland|FL|29208|58|0|136|8.16|||156.42|
-
+	# sample row
+	# 1|julia@roomm.gov|Linda|Garrett||8-(184)172-2138|10/19/2014||||||||||2354|1|Canceled||89 Cody Court||Woodland|FL|29208|58|0|136|8.16|||156.42|
+	query = """SELECT id, email, givenname, surname, telephone, called
+				FROM customers
+				WHERE called = ?"""
+	DB.execute(query, ("00/00/0000"))
+	row = DB.fetchone()
+	c = Customer(row[0], row[1], row[2], row[3], row[4], row[5])
+	return c
+	# display_next_to_call(c) << do this in main()
 
 
 
@@ -56,6 +60,20 @@ def update_customer_called(customer):
 
 
 def main():
+	done = False
+	connect_to_db()
+
+	while not done:
+		customer = get_next_customer()
+		display_next_to_call(customer)
+
+		print "Mark as called?"
+		answer = raw_input("> ")
+		if answer.lower() == "y":
+			update_customer_called(customer)
+		else:
+			break
+
 
 
 
